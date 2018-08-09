5158866803efb8585569e091886573f36a498d95
Authfix <3940727+Authfix@users.noreply.github.com>
2018-08-09 11:26:20 +0200

    Add docker secrets support for 1.6
    Add support for password environment variables INFLUXDB_ADMIN_PASSWORD,
    INFLUXDB_USER_PASSWORD, INFLUXDB_WRITE_USER_PASSWORD,
    INFLUXDB_READ_USER_PASSWORD

--- influxdb/1.6/init-influxdb.sh
+++ influxdb/1.6/init-influxdb.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -e
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
 AUTH_ENABLED="$INFLUXDB_HTTP_AUTH_ENABLED"
 
 if [ -z "$AUTH_ENABLED" ]; then
@@ -15,6 +37,11 @@ if ( [ ! -z "$INIT_USERS" ] || [ ! -z "$INFLUXDB_DB" ] || [ "$(ls -A /docker-ent
 
 	INIT_QUERY=""
 	CREATE_DB_QUERY="CREATE DATABASE $INFLUXDB_DB"
+
+	file_env 'INFLUXDB_ADMIN_PASSWORD'
+	file_env 'INFLUXDB_USER_PASSWORD'
+	file_env 'INFLUXDB_WRITE_USER_PASSWORD'
+	file_env 'INFLUXDB_READ_USER_PASSWORD'
 
 	if [ ! -z "$INIT_USERS" ]; then
 
